c2e559413dab6d0abda8cf94cc746ea094d42619
LienPaul <zinfu1111@gmail.com>
2021-09-24 13:37:51 +0800

    Initial Commit

---
+++ DemoDeviceInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoDeviceInfo
+//
+//  Created by 連振甫 on 2021/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
